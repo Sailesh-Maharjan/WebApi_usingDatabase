830892e4620b0d6d044e4fb6995a7c9d3dd1b1aa
Sailesh-Maharjan <maharjansailesh321@gmail.com>
2024-11-20 14:47:54 +0545

    Add project files.

---
+++ WebApi_usingDatabase/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi_usingDatabase.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string Gender { get; set; } = null!;
+}
